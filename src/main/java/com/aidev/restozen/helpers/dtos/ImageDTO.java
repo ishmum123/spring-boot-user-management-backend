caf45c27da21b735eceae73213e54b2854f69b5d
Ishmum Jawad Khan <ishmumkhan@google.com>
2024-03-14 16:18:17 +0600

    Add Extra Restrictions on Image Upload

--- src/main/java/com/aidev/restozen/helpers/dtos/ImageDTO.java
+++ src/main/java/com/aidev/restozen/helpers/dtos/ImageDTO.java
@@ -2,6 +2,14 @@ package com.aidev.restozen.helpers.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
-public record ImageDTO(@NotEmpty @Pattern(regexp = "^data:image/[a-z]+;base64,(.*)$") String image) {
+public record ImageDTO(
+        @NotEmpty
+        // base64 increases the bytes count by ~1.37.
+        // We are multiplying 10 mb by 1.4 (~1.37) here.
+        // ref: https://en.wikipedia.org/wiki/Base64
+        @Length(max = 14 * 1024 * 1024, message = "must be smaller than 10 mb")
+        @Pattern(regexp = "^data:image/[a-z]+;base64,(.*)$", message = "must contain an encoded image")
+        String image) {
 }
