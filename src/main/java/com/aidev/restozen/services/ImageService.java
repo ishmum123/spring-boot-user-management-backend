2e9455856cfab8dd6c4cd5dac406129792688db0
Ishmum Jawad Khan <ishmumkhan@google.com>
2024-03-16 16:46:06 +0600

    Remove `tmp` Images Older than 6 Hours

--- src/main/java/com/aidev/restozen/services/ImageService.java
+++ src/main/java/com/aidev/restozen/services/ImageService.java
@@ -6,14 +6,17 @@ import jakarta.servlet.ServletContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.Base64;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -25,20 +28,37 @@ public class ImageService {
     @Autowired
     private ServletContext context;
 
+    private static synchronized String createIdFromEpoch() {
+        return String.valueOf(Instant.now().toEpochMilli());
+    }
+
     public IdHolderDTO uploadImage(ImageDTO dto) {
-        UUID id = UUID.randomUUID();
+        String id = createIdFromEpoch();
         String imageData = dto.image();
 
         try {
             String fileType = imageData.split(IMAGE_PREFIX)[1].split(DATA_SEPARATOR)[0];
             byte[] bytes = Base64.getDecoder().decode(imageData.split(DATA_SEPARATOR)[1]);
             String directoryPath = new FileSystemResource("tmp").getFile().getAbsolutePath();
-            Path path = Paths.get(directoryPath , id + "." + fileType);
+            Path path = Paths.get(directoryPath, id + "." + fileType);
             Files.write(path, bytes);
-            return new IdHolderDTO(id.toString());
+            return new IdHolderDTO(id);
         } catch (IOException e) {
             // TODO: Create Action Specific RuntimeException-s
             throw new RuntimeException(e);
         }
+    }
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void removeStaleImages() {
+        String[] files = new FileSystemResource("tmp").getFile().list();
+        Arrays.stream(files)
+                .filter(file -> {
+                    String filenameEpoch = file.split("\\.")[0];
+                    long creationEpoch = Long.parseLong(filenameEpoch);
+                    Instant creationInstant = Instant.ofEpochMilli(creationEpoch);
+                    return Instant.now().isAfter(creationInstant.plus(Duration.ofHours(6)));
+                })
+                .forEach(file -> new FileSystemResource("tmp/" + file).getFile().delete());
     }
 }
